d4b5f745e75830a9061f579448c3b7a2d0b27963
Lukas Neukom <lukasneukom@MacBook-Pro.local>
2020-06-12 21:46:16 +0200

    working chess board

--- pythonTest.py
+++ pythonTest.py
@@ -7,19 +7,21 @@ window = Tk()
 window.title("DLM")
 window.geometry(str(WINDOW_HEIGHT) + 'x' + str(WINDOW_WIDTH))
 
-canvas = Canvas(window)
+canvas = Canvas(window, height=WINDOW_HEIGHT, width=WINDOW_WIDTH, bd=0, highlightthickness=0, relief='ridge')
 
 tilesX = WINDOW_WIDTH/10
 tilesY = WINDOW_HEIGHT/10
 
-for x in range(tilesX-1):
-    for y in range(tilesY-1):
+for x in range(tilesX):
+    for y in range(tilesY):
         if (x+y) % 2 == 0:
             canvas.create_rectangle(x*10, y*10, x*10+10, y*10+10, outline="#333", fill="#333")
         else:
             canvas.create_rectangle(x*10, y*10, x*10+10, y*10+10, outline="#ddd", fill="#ddd")
+            
+canvas.config()
 
-canvas.pack(fill=BOTH, expand=1)
+canvas.pack(fill=BOTH, expand=0)
 
 window.mainloop()
 
