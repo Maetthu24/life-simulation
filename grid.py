c6c44aaec5360149df05334259e87bb49e1e3ab7
Lukas Neukom <mail@lukasneukom.ch>
2020-06-12 23:54:43 +0200

    Draw entity on the grid

--- grid.py
+++ grid.py
@@ -1,17 +1,18 @@
 class Grid:
-	"""docstring for Grid"""
 	def __init__(self, tilesX, tilesY, tilesize, canvas):
 		self.tilesX = tilesX
 		self.tilesY = tilesY
 		self.tilesize = tilesize
 		self.canvas = canvas
 
-		
-
-
 		for x in range(self.tilesX):
-		    for y in range(self.tilesY):
-		        if (x+y) % 2 != 0:
-		            self.canvas.create_rectangle(x*10, y*10, x*10+10, y*10+10, outline="#333", fill="#333")
-		        else:
-		            self.anvas.create_rectangle(x*10, y*10, x*10+10, y*10+10, outline="#ddd", fill="#ddd")
+			for y in range(self.tilesY):
+				if (x+y) % 2 != 0:
+					self.canvas.create_rectangle(x*tilesize, y*tilesize, x*tilesize+tilesize, y*tilesize+tilesize, outline="", fill="#333")
+				else:
+					self.canvas.create_rectangle(x*tilesize, y*tilesize, x*tilesize+tilesize, y*tilesize+tilesize, outline ="", fill="#ddd")
+
+	def drawEntity(self, entity):
+		tilesize = self.tilesize
+		print(tilesize)
+		self.canvas.create_oval(entity.posX*tilesize, entity.posY*tilesize, entity.posX*tilesize+tilesize, entity.posY*tilesize+tilesize, fill=entity.color)
--- main.py
+++ main.py
@@ -1,35 +1,26 @@
 from tkinter import *
 from entity import Entity
+from grid import Grid
 import random
 
 if __name__ == '__main__':
 
 	WINDOW_HEIGHT = 500
 	WINDOW_WIDTH = 500
-
+	
 	window = Tk()
 	window.title("Life Simulation by Luki, Dave & Mätthe")
 	window.geometry(str(WINDOW_HEIGHT) + 'x' + str(WINDOW_WIDTH))
-
+	
 	canvas = Canvas(window, height=WINDOW_HEIGHT, width=WINDOW_WIDTH, bd=0, highlightthickness=0, relief='ridge')
-
-	tilesX = int(WINDOW_WIDTH/10)
-	tilesY = int(WINDOW_HEIGHT/10)
-
-	for x in range(tilesX):
-	    for y in range(tilesY):
-	        if (x+y) % 2 != 0:
-	            canvas.create_rectangle(x*10, y*10, x*10+10, y*10+10, outline="#333", fill="#333")
-	        else:
-	            canvas.create_rectangle(x*10, y*10, x*10+10, y*10+10, outline="#ddd", fill="#ddd")
-	            
-	canvas.config()
-
+	
+	g = Grid(10,10,40,canvas)
+	
 	canvas.pack(fill=BOTH, expand=0)
-
-	e1 = Entity(random.randint(0,tilesX-1), random.randint(0,tilesY-1))
-	print(e1.posX)
-	print(e1.posY)
-	canvas.create_oval()
-
+	e1 = Entity(random.randint(0,10-1), random.randint(0,10-1))
+	#    print(e1.posX)
+	#    print(e1.posY)
+	
+	g.drawEntity(e1)
+	
 	window.mainloop()
